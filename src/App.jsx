87d32f5ced20e7729ec8989ad587a1a43208d29e
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-03-05 06:50:04 +0530

    Create HTTP GET Function to Fetch Data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { getPost } from "./api/PostApi";
+
 function App() {
+  const getPostData = async () => {
+    const res = await getPost();
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
   return <h1>hekek</h1>;
 }
 
